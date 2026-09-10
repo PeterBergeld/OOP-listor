d077773fbaf3c50cc5375ae74c679c79396bbee4
Peter <peterbergeld@gmail.com>
2026-09-10 10:05:19 +0200

    Första commit till Del A

---
+++ DelA.cs
@@ -0,0 +1,12 @@
+// insköpslistor
+
+// Början med lista. Vi ska gör en lista
+// En för text List<T> == List<string>;
+// List<int> == för nummer
+//ingore.cs ska läggas till 
+//obj och ____ 
+List<T> == List<string>;
+List<int> == List<int>;
+
+name[0] = "Mjölk, Vatten, Mannagryn";
+prices[0] = 20 + 30 + 50; // Går detta ens ? hmm
