808c667a54e37e314d39f0c83e35dcb1b4d8ccdf
Peter <peterbergeld@gmail.com>
2026-09-12 08:31:11 +0200

    DELA2 (1)

---
+++ DELA2.cs
@@ -0,0 +1,9 @@
+// Börja om från början, börja om på nytt, varför ska man......
+
+
+List<string> names = new List<string>(); // an expression req and argument list or in this case ()
+List<int> priser = new List<int>(); //Fortfarande lite konfys angående att grey visas under name innan den värderas, kolla upp colour.
+
+names.Add("Mjölk");
+priser.Add(15);
+
