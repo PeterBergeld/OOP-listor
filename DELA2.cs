0257f54c3391a777d995dbe959c77a13822246c1
Peter <peterbergeld@gmail.com>
2026-09-12 08:54:20 +0200

    (2)

--- DELA2.cs
+++ DELA2.cs
@@ -6,4 +6,17 @@ List<int> priser = new List<int>(); //Fortfarande lite konfys angående att grey
 
 names.Add("Mjölk");
 priser.Add(15);
+names.Add("Bröd");
+priser.Add(32);
+names.Add("Ost");
+priser.Add(89);
+int summa = 0;
 
+for (int i =0; i< names.Count; i++)  // Visa varan och priset därav en for-loop
+{
+   
+    System.Console.WriteLine($"{i+1}. {names[i]} - {priser[i]}");
+    //System.Console.WriteLine($"{i+1}.{priser[i]}"); // Behöver inte vara med , för att de ska kopplas ihop
+    
+}
+    
